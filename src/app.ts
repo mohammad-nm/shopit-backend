2b7ef8d7e85c182c8182ac90393dc400600a1cfd
Mohammad Nickmagham <mohammadnickmagham@gmail.com>
2024-12-13 22:48:06 +0330

    FIX

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://shopit-backend-one.vercel.app",
+    origin: "https://shopit-tau.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
