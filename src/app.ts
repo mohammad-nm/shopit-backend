d93704156e2fb404efdf8bcf873c236ad6fe0cef
Mohammad Nickmagham <mohammadnickmagham@gmail.com>
2024-12-13 23:47:23 +0330

    FIX

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,19 @@ const app: Application = express();
 
 app.use(express.json());
 
+// app.use(
+//   cors({
+//     origin: ["https://shopit-tau.vercel.app", "http://localhost:3000"],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
     origin: "https://shopit-tau.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    methods: "GET,POST",
+    allowedHeaders: "Content-Type,Authorization",
   })
 );
 
