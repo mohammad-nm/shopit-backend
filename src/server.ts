e28c610860e909b90837237d380a0211241af0fd
Mohammad Nickmagham <mohammadnickmagham@gmail.com>
2024-12-17 19:32:56 +0330

    ADD COSR policy for local

--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,3 @@
-// import dotenv from "dotenv";
-// import connectDB from "./config/db";
-// import app from "./app";
-// import signinController from "./controllers/signinController";
-// import signupController from "./controllers/signupController";
-// import getProducts, {
-//   getProductById,
-//   getProductsByIds,
-// } from "./controllers/productsController";
-// import cartUpdate from "./controllers/cartUpdate";
-// import cors from "cors";
-
-// app.use(
-//   cors({
-//     origin: "https://shopit-tau.vercel.app",
-//     methods: "GET,POST",
-//     allowedHeaders: "Content-Type,Authorization",
-//   })
-// );
-// dotenv.config();
-
-// const PORT = process.env.PORT || 3000;
-
-// connectDB();
-
-// // app.use("/api", signin);
-// // app.use("/api", signup);
-// app.get("/api/test", (req, res) => {
-//   res.send("API is running...");
-// });
-// app.post("/api/signin", signinController);
-// app.post("/api/signup", signupController);
-// app.post("/api/products", getProducts);
-// app.post("/api/product", getProductById);
-// app.post("/api/cart", cartUpdate);
-// app.post("/api/products/ids", getProductsByIds);
-// // app.listen(PORT, () => {
-// //   console.log(`Server is running on port ${PORT}`);
-// // });
-// export default app;
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
@@ -58,6 +18,13 @@ const app = express();
 app.use(
   cors({
     origin: "https://shopit-tau.vercel.app", // Your frontend URL
+    methods: "GET,POST",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
+app.use(
+  cors({
+    origin: "http://localhost:3000",
     methods: "GET,POST",
     allowedHeaders: "Content-Type,Authorization",
   })
